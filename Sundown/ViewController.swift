3fe35be23d9afbe546fa8f644c4c3277c93f0537
Carolyn Tran <carolyn.tran@outlook.com>
2017-06-30 00:50:24 -0700

    initial structure

---
+++ Sundown/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sundown
+//
+//  Created by Carolyn Tran on 6/29/17.
+//  Copyright © 2017 Carolyn Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
